dadfd442d40b522f6c22c460c3c175bd5043cb19
Mathew Ostrander <mathew.ostrander@gmail.com>
2015-12-17 14:15:31 -0600

    about to try to reformat my dropdown options

--- app/controllers/memberController.js
+++ app/controllers/memberController.js
@@ -1,6 +1,6 @@
 app.controller("memberController",
-	["$scope", "$rootScope", "fireFactory", "$firebaseObject",
-	function($scope, $rootScope, fireFactory, $firebaseObject) {
+	["$scope", "$rootScope", "fireFactory", "userFactory", "$firebaseObject",
+	function($scope, $rootScope, fireFactory, userFactory, $firebaseObject) {
 
 		$scope.editMode;
 		var memberUid = fireFactory.getUid();
@@ -8,11 +8,31 @@ app.controller("memberController",
 
     var profileRef = new Firebase("https://relay-radar.firebaseio.com/user/"+memberUid);
    	$scope.memberProfile = $firebaseObject(profileRef);
+   	console.log("memberProfile", $scope.memberProfile);
+
+		$scope.hobbyArray = userFactory.getHobbyArray();
+		$scope.colorArray = userFactory.getColorArray();
+		$scope.favoriteColor = $scope.colorArray[0];
+ 		$scope.favoriteHobby = $scope.hobbyArray[26];
+
+
+      $scope.editUser = {
+      // zip: $scope.memberProfile.zip,
+      colorFave: $scope.favoriteColor.selectedColor,
+      hobbyFave: $scope.favoriteHobby.selectedhobby,
+    //   Gender: 'a',
+	   //  Birthday: 'a',
+	   //  Friendcode: 'a',
+	   //  Games: 'a',
+		  // Records: 'a'
+    };
+
 
    	$scope.toggleEditMode = function(){
       if ($scope.editMode) {
         $scope.editMode = false;
         console.log("no longer in edit Profile mode");
+        console.log("editUser", $scope.memberProfile);
       } else {
         $scope.editMode = true;
         console.log("Edit Profile mode");
